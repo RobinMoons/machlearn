db5a0caa7423071e41bf55581cb2d81e44037a38
Bram van Deurzen <bram.vandeurzen@student.uhasselt.be>
2017-05-02 22:32:08 +0200

    Alle andere technieken die we moeten gebruiken ook toegevoegd. Heb k-nearest neighbour gedaan met de optimale k die bepaald wordt door matlab. Dus duurt het uitvoeren hiervan wel een stuk langer.

--- test_read_data.m
+++ test_read_data.m
@@ -237,3 +237,132 @@ plot(OPTROCPT(1),OPTROCPT(2),'ro')
 xlabel('False positive rate')
 ylabel('True positive rate')
 title('ROC Curve for Classification by Classification SVM linear')
+
+%% Naive bayes as Binary classificcation
+% Meshgrid
+d = 0.1;
+[x1Grid,x2Grid] = meshgrid(min(featureMatrix(:,1)):d:max(featureMatrix(:,1)),...
+    min(featureMatrix(:,2)):d:max(featureMatrix(:,2)));
+xGrid = [x1Grid(:),x2Grid(:)];
+
+% Classification
+bayesTree = fitcnb(featureMatrix,Class);
+
+bayesTree
+bayesTree.DistributionParameters
+Params = cell2mat(bayesTree.DistributionParameters);
+
+Mu = Params([1 3],1:2); % Extract the means
+Sigma = zeros(2,2,3);
+
+face = {'r' , 'b' }
+
+for j = 1:2
+    Sigma(:,:,j) = diag(Params(2*j,:)).^2; % Create diagonal covariance matrix
+    
+    surf(x1Grid,x2Grid,reshape(mvnpdf(xGrid,Mu(j,:),Sigma(:,:,j)),size(x1Grid)),'FaceAlpha',0.5,'FaceColor',face{j})
+        % Draw contours for the multivariate normal distributions
+end
+
+% Accuracy on trainings data
+%help resubPredict
+[Cpred_tr,score,node] = resubPredict(bayesTree);
+%help confusionmat
+C_bayes_tr = confusionmat(Class,Cpred_tr)
+accuracy = trace(C_bayes_tr)/sum(sum(C_bayes_tr))
+
+[Cpred,score] = predict(bayesTree,testFeatureMatrix(:,4:5));
+C_bayes_te = confusionmat(Clte,Cpred)
+accuracy = trace(C_bayes_te)/sum(sum(C_bayes_te))
+
+% visualisation of results
+labels = predict(bayesTree,xGrid);
+
+% Training data points
+figure('Name', 'Naive Bayes - 2D division feature curve trainingsdata', 'NumberTitle', 'off')
+h(1:2) = gscatter(xGrid(:,1),xGrid(:,2),labels,[0.1 0.5 0.5; 0.5 0.1 0.5 ]);
+hold on
+h(3:4) = gscatter(featureMatrix(:,1),featureMatrix(:,2),Class);
+legend(h,{'Class1','Class2','Class1 Tr','Class2 Tr'},...
+   'Location','Northwest');
+xlabel('x1');
+ylabel('x2');
+
+% Testing data points
+figure('Name', 'Naive Bayes - 2D division feature curve testdata (from testData.mat)', 'NumberTitle', 'off')
+h(1:2) = gscatter(xGrid(:,1),xGrid(:,2),labels,[0.1 0.5 0.5; 0.5 0.1 0.5 ]);
+hold on
+h(3:4) = gscatter(testFeatureMatrix(:,4),testFeatureMatrix(:,5),Clte);
+legend(h,{'Class1','Class2','Class1 Te','Class2 Te'},...
+   'Location','Northwest');
+xlabel('x1');
+ylabel('x2');
+
+% ROC curve one vs one
+[fpr,tpr,T,AUC,OPTROCPT] = perfcurve(Clte,score(:,1),1);
+AUC
+figROC=figure;
+plot(fpr,tpr,'.-')
+hold on
+plot(OPTROCPT(1),OPTROCPT(2),'ro')
+xlabel('False positive rate')
+ylabel('True positive rate')
+title('ROC Curve for Classification by Naive Bayes')
+
+%% K-nearest neighbour as Binary Classification
+% meshgrid
+d = 0.1;
+[x1Grid,x2Grid] = meshgrid(min(featureMatrix(:,1)):d:max(featureMatrix(:,1)),...
+    min(featureMatrix(:,2)):d:max(featureMatrix(:,2)));
+xGrid = [x1Grid(:),x2Grid(:)];
+
+% knn Classifier k == 1 dist = euclidian
+% Voor: Mdl = fitcknn(Xtr,Cltr);
+
+% knn Classifier k == opt dist = opt
+knnTree = fitcknn(featureMatrix,Class,'OptimizeHyperparameters','auto')
+knnTree
+
+% Accuracy on trainings data
+[Cpred_tr,score,node] = resubPredict(knnTree);
+C_knear_tr = confusionmat(Class,Cpred_tr)
+accuracy = trace(C_knear_tr)/sum(sum(C_knear_tr))
+
+% Accuracy on test data
+[Cpred,score] = predict(knnTree,testFeatureMatrix(:,4:5));
+C_knear_te = confusionmat(Clte,Cpred)
+accuracy = trace(C_knear_te)/sum(sum(C_knear_te))
+
+% visualisation of results
+labels = predict(knnTree,xGrid);
+
+% Training data points
+figure('Name', 'K-nearest neighbor - 2D division feature curve trainingsdata', 'NumberTitle', 'off')
+h(1:2) = gscatter(xGrid(:,1),xGrid(:,2),labels,[0.1 0.5 0.5; 0.5 0.1 0.5 ]);
+hold on
+h(3:4) = gscatter(featureMatrix(:,1),featureMatrix(:,2),Class);
+legend(h,{'Class1','Class2','Class1 Tr','Class2 Tr'},...
+   'Location','Northwest');
+xlabel('x1');
+ylabel('x2');
+
+% Testing data points
+figure('Name', 'K-nearest neighbor - 2D division feature curve testdata (from testData.mat)', 'NumberTitle', 'off')
+h(1:2) = gscatter(xGrid(:,1),xGrid(:,2),labels,[0.1 0.5 0.5; 0.5 0.1 0.5 ]);
+hold on
+h(3:4) = gscatter(testFeatureMatrix(:,4),testFeatureMatrix(:,5),Clte);
+legend(h,{'Class1','Class2','Class1 Te','Class2 Te'},...
+   'Location','Northwest');
+xlabel('x1');
+ylabel('x2');
+
+% ROC curve one vs one
+[fpr,tpr,T,AUC,OPTROCPT] = perfcurve(Clte,score(:,1),1);
+AUC
+figure;
+plot(fpr,tpr,'.-')
+hold on
+plot(OPTROCPT(1),OPTROCPT(2),'ro')
+xlabel('False positive rate')
+ylabel('True positive rate')
+title('ROC Curve for Classification by knn')
